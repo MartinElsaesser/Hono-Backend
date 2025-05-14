49cd5e1fe35637ea1eb27948c7bd3224f16b574d
Martin Elsaesser <marels@aon.at>
2025-05-14 21:04:20 +0200

    add logic for swapping todo positions

--- src/routers/apiRouter.tsx
+++ src/routers/apiRouter.tsx
@@ -2,7 +2,10 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 import { db } from "../db/db.js";
-import { parsePositiveIntSchema } from "../schemas/utilitySchemas.js";
+import {
+  parsePositiveIntSchema,
+  positiveIntSchema,
+} from "../schemas/utilitySchemas.js";
 
 const apiRouter = new Hono()
   // get all todos
@@ -65,6 +68,126 @@ const apiRouter = new Hono()
       return c.json({
         success: true,
         data: { post },
+      });
+    }
+  )
+  // swap todos by positions
+  .patch(
+    "/todos/swap1",
+    zValidator(
+      "json",
+      z.object({
+        position1: positiveIntSchema,
+        position2: positiveIntSchema,
+      })
+    ),
+    async (c) => {
+      const { position1, position2 } = await c.req.valid("json");
+
+      const [todo1, todo2] = await db
+        .selectFrom("todo")
+        .select(["id", "position"])
+        .where("position", "in", [position1, position2])
+        .execute();
+
+      const result = await db.transaction().execute(async (trx) => {
+        const swappedTodo1 = await trx
+          .updateTable("todo")
+          .set({ position: todo2.position })
+          .where("id", "=", todo1.id)
+          .returningAll()
+          .executeTakeFirstOrThrow();
+
+        const swappedTodo2 = await trx
+          .updateTable("todo")
+          .set({ position: todo1.position })
+          .where("id", "=", todo2.id)
+          .returningAll()
+          .executeTakeFirstOrThrow();
+
+        return [swappedTodo1, swappedTodo2];
+      });
+
+      return c.json({
+        success: true,
+        data: { result },
+      });
+    }
+  )
+  // swap todos by ids
+  .patch(
+    "/todos/swap3",
+    zValidator(
+      "json",
+      z.object({
+        id1: positiveIntSchema,
+        id2: positiveIntSchema,
+      })
+    ),
+    async (c) => {
+      const { id1, id2 } = await c.req.valid("json");
+
+      const [todo1, todo2] = await db
+        .selectFrom("todo")
+        .select(["id", "position"])
+        .where("id", "in", [id1, id2])
+        .execute();
+
+      const result = await db.transaction().execute(async (trx) => {
+        const swappedTodo1 = await trx
+          .updateTable("todo")
+          .set({ position: todo2.position })
+          .where("id", "=", todo1.id)
+          .returningAll()
+          .executeTakeFirstOrThrow();
+
+        const swappedTodo2 = await trx
+          .updateTable("todo")
+          .set({ position: todo1.position })
+          .where("id", "=", todo2.id)
+          .returningAll()
+          .executeTakeFirstOrThrow();
+
+        return [swappedTodo1, swappedTodo2];
+      });
+
+      return c.json({
+        success: true,
+        data: { result },
+      });
+    }
+  )
+  // swap todos by position
+  .patch(
+    "/todos/swap2",
+    zValidator(
+      "json",
+      z.object({
+        position1: positiveIntSchema,
+        position2: positiveIntSchema,
+      })
+    ),
+    async (c) => {
+      const { position1, position2 } = await c.req.valid("json");
+
+      const result = await db
+        .updateTable("todo")
+        .set((eb) => ({
+          position: eb
+            .case()
+            .when("position", "=", position1)
+            .then(position2)
+            .else(position1)
+            .end(),
+        }))
+        .where("position", "in", [position1, position2])
+        .returningAll()
+        .execute();
+      console.log(result);
+
+      return c.json({
+        success: true,
+        data: { compiled: result },
       });
     }
   )
